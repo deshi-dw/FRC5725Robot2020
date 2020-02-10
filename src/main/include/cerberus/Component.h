469e0b58ed7292c3ea41af8b132447cf60692b2a
deshi-dw <43869473+deshi-dw@users.noreply.github.com>
2020-02-10 08:03:37 -0700

    Component system changes

--- src/main/include/cerberus/Component.h
+++ src/main/include/cerberus/Component.h
@@ -1,8 +1,28 @@
 #pragma once
 
+enum State {
+    DISABLED,
+    TESTING,
+    AUTONOMOUS,
+    TELEOP
+};
+
+static State c_robotState;
+
 class Component {
-	public:
-	virtual void update() = 0;
-	virtual void initialize() = 0;
+   public:
+    virtual void update() = 0;
+    virtual void initialize() = 0;
     virtual void deinitialize() = 0;
+
+    virtual bool updateCondition() {
+        return true;
+    }
+
+    bool isInitialized() {
+        return m_isInitialized;
+    }
+
+   protected:
+    bool m_isInitialized = false;
 };
