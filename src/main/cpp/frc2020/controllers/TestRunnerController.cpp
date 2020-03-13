0afb6fe9c047be507c0372e812a8b35c46acaf1d
deshi-dw <deshi-dw@gmail.com>
2020-03-13 13:23:03 -0600

    made TestRunnerController mode modular

--- src/main/cpp/frc2020/controllers/TestRunnerController.cpp
+++ src/main/cpp/frc2020/controllers/TestRunnerController.cpp
@@ -27,6 +27,80 @@ void TestRunnerController::initialize() {
     Robot::inputs->add<InputDigital>(&buttonPrev);
 }
 
+bool TestRunnerController::isAPressed() {
+    return buttonA.value && previousIsInputDown == false;
+}
+bool TestRunnerController::isBPressed() {
+    return buttonA.value && previousIsInputDown == false;
+}
+bool TestRunnerController::isXPressed() {
+    return buttonA.value && previousIsInputDown == false;
+}
+bool TestRunnerController::isYPressed() {
+    return buttonA.value && previousIsInputDown == false;
+}
+
+void TestRunnerController::change_value(bool is_up, bool is_down, double* value, double increment, double min, double max, const std::string& value_name) {
+    if (is_up == true) {
+        *value = std::min(*value + increment, max);
+    } else if (is_down == true) {
+        *value = std::max(*value - increment, min);
+    } else {
+        return;
+    }
+    Robot::logger->println("%s = %f", value_name, value);
+}
+
+bool TestRunnerController::change_mode(bool is_up, int* mode, int mode_count) {
+    if (is_up == true) {
+        *mode = std::abs(*mode + 1) % mode_count;
+    } else {
+        return false;
+    }
+
+    return true;
+}
+
+void TestRunnerController::print_controls(const std::string& task_name) {
+    Robot::logger->println("X = start/stop %s.", task_name);
+    Robot::logger->println("B = select mode.");
+    Robot::logger->println("Y = increase value.");
+    Robot::logger->println("A = decrease value.");
+}
+
+void TestRunnerController::print_mode(int mode, const std::string& mode_name, bool is_sub) {
+    if (is_sub == true) {
+        Robot::logger->println("submode %i [%s]", mode, mode_name);
+    } else {
+        Robot::logger->println("mode %i [%s]", mode, mode_name);
+    }
+}
+
+void TestRunnerController::start_and_stop(std::function<void()> func) {
+    if (isAPressed()) {
+        if (start == false) {
+            startTime = Robot::getRobotTime();
+            start = true;
+        } else {
+            startTime = 0;
+            drivetrain->driveTank(0, 0);
+            intake->setState(Intake::IDLE);
+            start = false;
+        }
+    }
+
+    // if the timer has started,
+    if (start == true) {
+        // drive forwards for 1000 ms (1 sec).
+        if (Robot::getRobotTime() - startTime <= test_duration) {
+            func();
+        } else {
+            start = false;
+            drivetrain->driveTank(0, 0);
+        }
+    }
+}
+
 void TestRunnerController::deinitialize() {}
 
 void TestRunnerController::update() {
@@ -74,106 +148,82 @@ bool TestRunnerController::condition() {
 
 void TestRunnerController::test_drive1(bool is_init, bool in_a, bool in_b, bool in_x, bool in_y) {
     if (is_init) {
-        Robot::logger->println("TEST MODE %i [DRIVE1]", selectedMode);
-        Robot::logger->println("X = start driving forwards at drive_speed.");
-        Robot::logger->println("B = stop the robot.");
-        Robot::logger->println("Y = increase drive speed by 0.25");
-        Robot::logger->println("A = decrease drive speed by 0.25");
+        print_mode(selectedMode, "DRIVE_STRAIGHT", false);
+        print_controls("driving straight");
+
+        b_mode = 0;
     }
 
-    /*
-		* X = start driving forwards at drive_speed.
-		* B = stop the robot and drive timer.
-		* Y = increase drive speed by 0.25
-		* A = decrease drive speed by 0.25
-		*/
+    start_and_stop([&]() -> void { drivetrain->driveArcade(drive_speed, 0.0); });
 
-    // if x is press, start the robot.
-    if (in_x == true && previousIsInputDown == false && start == false) {
-        startTime = Robot::getRobotTime();
-        start = true;
-    }
-
-    // if the timer has started,
-    if (start == true) {
-        // drive forwards for 1000 ms (1 sec).
-        if (Robot::getRobotTime() - startTime <= 1.000) {
-            drivetrain->driveArcade(drive_speed, 0.0);
-        } else {
-            start = false;
-            drivetrain->driveTank(0, 0);
+    // if b is pressed, stop the robot.
+    if (change_mode(in_b, &b_mode, 2)) {
+        if (b_mode == 0) {
+            print_mode(b_mode, "SET SPEED", true);
+        } else if (b_mode == 1) {
+            print_mode(b_mode, "SET TIME(sec)", true);
         }
     }
 
-    // if b is pressed, stop the robot.
-    if (in_b == true) {
-        start = false;
-        startTime = 0;
-
-        drivetrain->driveTank(0, 0);
-    }
-
-    // if y is pressed, increase drive speed.
-    if (in_y == true && previousIsInputDown == false) {
-        drive_speed = std::min(drive_speed + 0.25, 1.0);
-        Robot::logger->println("drive_speed = %f", drive_speed);
-    }
-    // if a is pressed, decrease drive speed.
-    else if (in_a == true && previousIsInputDown == false) {
-        drive_speed = std::max(drive_speed - 0.25, -1.0);
-        Robot::logger->println("drive_speed = %f", drive_speed);
+    if (b_mode == 0) {
+        change_value(isYPressed(), isAPressed(), &drive_speed, speed_increment, -1.0, 1.0, "drive_speed");
+    } else if (b_mode == 1) {
+        change_value(isYPressed(), isAPressed(), &test_duration, time_increment, 0.0, 5.0, "test_duration");
     }
 }
 
 void TestRunnerController::test_drive2(bool is_init, bool in_a, bool in_b, bool in_x, bool in_y) {
     if (is_init) {
-        Robot::logger->println("TEST MODE %i [DRIVE2]", selectedMode);
-        Robot::logger->println("X = start turning at turn_speed.");
-        Robot::logger->println("B = stop the robot.");
-        Robot::logger->println("Y = increase turn speed by 0.25");
-        Robot::logger->println("A = decrease turn speed by 0.25");
+        print_mode(selectedMode, "SPIN", false);
+        print_controls("spin");
+
+        b_mode = 0;
     }
 
-    /*
-		* X = start turning at turn_speed.
-		* B = stop the robot and drive timer.
-		* Y = increase turn speed by 0.25
-		* A = decrease turn speed by 0.25
-		*/
+    start_and_stop([&]() -> void { drivetrain->driveArcade(0.0, drive_speed); });
 
-    // if x is press, start the robot.
-    if (in_x == true && previousIsInputDown == false && start == false) {
-        startTime = Robot::getRobotTime();
-        start = true;
-    }
-
-    // if the timer has started,
-    if (start == true) {
-        // turn for 1000 ms (1 sec).
-        if (Robot::getRobotTime() - startTime <= 1.000) {
-            drivetrain->driveArcade(0.0, turn_speed);
-        } else {
-            start = false;
-            drivetrain->driveTank(0, 0);
+    // if b is pressed, stop the robot.
+    if (change_mode(in_b, &b_mode, 2)) {
+        if (b_mode == 0) {
+            print_mode(b_mode, "SET SPEED", true);
+        } else if (b_mode == 1) {
+            print_mode(b_mode, "SET TIME(sec)", true);
         }
     }
 
+    if (b_mode == 0) {
+        change_value(isYPressed(), isAPressed(), &turn_speed, speed_increment, -1.0, 1.0, "turn_speed");
+    } else if (b_mode == 1) {
+        change_value(isYPressed(), isAPressed(), &test_duration, time_increment, 0.0, 5.0, "test_duration");
+    }
+}
+
+void TestRunnerController::test_drive3(bool is_init, bool in_a, bool in_b, bool in_x, bool in_y) {
+    if (is_init) {
+        print_mode(selectedMode, "ARC", false);
+        print_controls("arc");
+
+        b_mode = 0;
+    }
+
+    start_and_stop([&]() -> void { drivetrain->driveArcade(0.0, drive_speed); });
+
     // if b is pressed, stop the robot.
-    if (in_b == true) {
-        start = false;
-        startTime = 0;
-
-        drivetrain->driveTank(0, 0);
+    if (change_mode(in_b, &b_mode, 3)) {
+        if (b_mode == 0) {
+            print_mode(b_mode, "SET RIGHT SPEED", true);
+        } else if (b_mode == 1) {
+            print_mode(b_mode, "SET LEFT SPEED", true);
+        } else if (b_mode == 2) {
+            print_mode(b_mode, "SET TIME(sec)", true);
+        }
     }
 
-    // if y is pressed, increase turn speed.
-    if (in_y == true && previousIsInputDown == false) {
-        turn_speed = std::min(turn_speed + 0.25, 1.0);
-        Robot::logger->println("turn_speed = %f", turn_speed);
-    }
-    // if a is pressed, decrease turn speed.
-    else if (in_a == true && previousIsInputDown == false) {
-        turn_speed = std::max(turn_speed - 0.25, -1.0);
-        Robot::logger->println("turn_speed = %f", turn_speed);
+    if (b_mode == 0) {
+        change_value(isYPressed(), isAPressed(), &right_speed, speed_increment, -1.0, 1.0, "right_speed");
+    } else if (b_mode == 1) {
+        change_value(isYPressed(), isAPressed(), &left_speed, speed_increment, -1.0, 1.0, "left_speed");
+    } else if (b_mode == 1) {
+        change_value(isYPressed(), isAPressed(), &test_duration, time_increment, 0.0, 5.0, "test_duration");
     }
 }
--- src/main/include/frc2020/controllers/TestRunnerController.h
+++ src/main/include/frc2020/controllers/TestRunnerController.h
@@ -10,6 +10,9 @@ struct InputDigital;
 
 #include <cerberus/Event.h>
 
+#include <functional>
+#include <string>
+
 using namespace cerberus;
 
 class TestRunnerController : public Event {
@@ -43,13 +46,36 @@ class TestRunnerController : public Event {
 
     bool condition();
 
+    bool isAPressed();
+    bool isBPressed();
+    bool isXPressed();
+    bool isYPressed();
+
+    void change_value(bool is_up, bool is_down, double* value, double increment, double min, double max, const std::string& value_name);
+    bool change_mode(bool is_up, int* mode, int mode_count);
+
+    void print_controls(const std::string& task_name);
+    void print_mode(int mode, const std::string& mode_name, bool is_sub);
+
+    void start_and_stop(std::function<void()> func);
+
     bool start = false;
     double startTime = 0;
+
+    double speed_increment = 0.1;
+    double time_increment = 0.1;
 
     double drive_speed = 0.5;
     double turn_speed = 0.5;
 
-    void test_drive1(bool is_init, bool in_a, bool in_b, bool in_x, bool in_y);
+    double right_speed = 0.5;
+    double left_speed = 0.5;
 
+    double test_duration = 1.000;
+
+    int b_mode = 0;
+
+    void test_drive1(bool is_init, bool in_a, bool in_b, bool in_x, bool in_y);
     void test_drive2(bool is_init, bool in_a, bool in_b, bool in_x, bool in_y);
+    void test_drive3(bool is_init, bool in_a, bool in_b, bool in_x, bool in_y);
 };
